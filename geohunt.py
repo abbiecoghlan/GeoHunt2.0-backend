8af2a81e3099aae9688c19f2fc560803a387a0b4
“Abbie <abbie.coghlan@gmail.com>
2021-05-24 18:44:07 -0500

    setting up db

--- geohunt.py
+++ geohunt.py
@@ -1,7 +1,17 @@
-from flask import Flask
+from flask import Flask, request
+from flask_sqlalchemy import SQLAlchemy 
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'password'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
+db = SQLAlchemy(app)
 
+class UserModel(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(12))
+
+    def __str__(self):
+        return f'{self.content, {self.id}}'
 
 @app.route('/')
 def hello():
