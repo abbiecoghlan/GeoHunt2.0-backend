0c633c14be741ea5cb71b36ab4af5997ea8c8bc8
“Abbie <abbie.coghlan@gmail.com>
2021-05-24 17:18:11 -0500

    updated geohunt.py

--- geohunt.py
+++ geohunt.py
@@ -7,6 +7,15 @@ app = Flask(__name__)
 def hello():
     return 'Welcome to Geo Hunt!'
 
+@app.route('/<name>')
+def greet(name):
+    return f'Hello {name}'
+
 @app.route('/another')
 def anotherPage():
     return 'Welcome to another Geo Hunt page!'
+
+## this code calls the app.run function and executes the code in this file
+## now you can call python geohunt.py in the venv and it will run flask
+if __name__ == '__main__':
+    app.run(debug=True)
