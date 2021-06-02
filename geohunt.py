f1119dfa07cebf3359b0957d9de714bf0bc19ae1
Marc <marc.ferraro1@gmail.com>
2021-06-02 15:59:32 -0500

    db deleted, usermodel - user

--- geohunt.py
+++ geohunt.py
@@ -6,7 +6,7 @@ app.config['SECRET_KEY'] = 'password'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///tmp/test.db'
 db = SQLAlchemy(app)
 
-class UserModel(db.Model):
+class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(12))
 
